42a7b6d647f407563156b071619000870f81e58a
Michael Skogberg <malliina123@gmail.com>
2022-02-28 18:41:00 +0200

    Init

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Michael Skogberg on 28.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
